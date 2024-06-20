b20ea9045e7ff6a96248d79f11ccefa9896b1877
Jin Park <jinpark1504@gmail.com>
2024-06-20 09:11:01 +0900

    Initial Commit

---
+++ SignUpForm2/SignUpForm2App.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpForm2App.swift
+//  SignUpForm2
+//
+//  Created by Jungjin Park on 2024-06-20.
+//
+
+import SwiftUI
+
+@main
+struct SignUpForm2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
